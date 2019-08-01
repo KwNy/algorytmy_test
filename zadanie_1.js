6c605f17d3bc986a5b86b490f76825b7099920b5
KwNy <pactwa.k@gmail.com>
2019-08-01 15:20:07 +0200

    bubble sort dleted

--- zadanie_1.js
+++ zadanie_1.js
@@ -52,34 +52,4 @@ function merge(arrA, arrB){
 //bubble sort 
 
 
-function bubbleSort(array) {
-
-  let Issorted = false; 
-  let len = array.length;
-  console.log(array.length)
-
-  while(Issorted===false){
-
-    console.log('waiting to sort')
-
-    for(let i = 0; i < len-1; i++){
-
-    console.log('unsorted list start at', i, 'ends at', len)
-
-      if(array[i]>array[i+1]){
-        temp = array[i+1];
-        array[i+1] = array[i];
-        array[i] = temp;
-      }
-    }
-    Issorted = true;
-  }
-  return array
-}
-
-console.log(bubbleSort([1,5,2,8,7]))
-
-// https://www.youtube.com/watch?v=6Gv8vg0kcHc
-
-//dlaczego musi sobie odejmuje --- length, przeciez nie dojdzie nigsy do ostatniego elementu 
 
